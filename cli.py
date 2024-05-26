945ce00e129c6873e75e778caa0449ca8951e24d
Gergő Miszori <miszorigergo@gmail.com>
2024-05-26 08:54:11 +0200

    fine tuning

--- cli.py
+++ cli.py
@@ -8,5 +8,5 @@ x = deque(maxlen=50)
 while True:
     x.append(detection())
     most_common = [mode([i[j] for i in x]) for j in range(len(x[0]))]
-    print(f"The most common element is: {most_common} Latest: {x[-1]}")
+    print(f"Status: {most_common} ({x[-1]})")
     sleep(1)
